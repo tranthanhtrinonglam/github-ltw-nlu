a091e6b7ce5610a5fa9388a38beb3cc4404386e3
tranthanhtrinonglam <145947346+tranthanhtrinonglam@users.noreply.github.com>
2023-10-15 23:32:46 +0700

    Add files via upload

---
+++ lab5/src/main/java/com/example/lab5_1/SignInServlet.java
@@ -0,0 +1,26 @@
+package com.example.lab5_1;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "SignInServlet", urlPatterns = {"/signin"})
+public class SignInServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/signin.html");
+        dispatcher.forward(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        resp.sendRedirect("signin.html");
+    }
+}
---
+++ lab5/src/main/java/com/example/lab5_1/SignUpServlet.java
@@ -0,0 +1,26 @@
+package com.example.lab5_1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "SignUpServlet", urlPatterns ={"/signup"})
+public class SignUpServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/signup.html");
+        dispatcher.forward(req, resp);
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        response.sendRedirect("signup.html");
+    }
+}
